39106d58b937eea090c34d4ecc30a03a88b86807
jota <jorgeoterodelrio@gmail.com>
2016-06-09 09:18:28 +0200

    Modificación para pedir tamaño y clave por consola

--- src/busqueda/Busqueda.java
+++ src/busqueda/Busqueda.java
@@ -1,15 +1,21 @@
 package busqueda;
 
+import java.util.Scanner;
+
 public class Busqueda {
 
     public static void main(String[] args) {
-        int[] numeros = new int[100];
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca el tamaño del array:");
+        int[] numeros = new int[teclado.nextInt()];
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = i * 5;
         }
         Busqueda ejemplo = new Busqueda();
-        int indice = ejemplo.busquedaBinaria(numeros, 450, 0, numeros.length - 1);
-        System.out.println("El indice del valor '450' es: " + indice);
+        System.out.println("Que número desea buscar en el array:");
+        int clave = teclado.nextInt();
+        int indice = ejemplo.busquedaBinaria(numeros, clave, 0, numeros.length - 1);
+        System.out.println("El indice del valor '"+clave+"' es: " + indice);
     }
 
     public int busquedaBinaria(int[] listado, int clave, int posInicial, int posFinal) {
